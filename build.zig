040d087984247b629367ce05bee9c8722c1d312b
Ethan Evans <ethanalexevans@gmail.com>
2023-10-25 00:04:27 -0700

    Raylib Hello

---
+++ build.zig
@@ -0,0 +1,65 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const raylib_prebuild = b.addSystemCommand(&.{
+        "cmake",
+        "-S", "deps/raylib",
+        "-B", "deps/raylib/build",
+    });
+
+    const raylib_build = b.addSystemCommand(&.{
+        "cmake",
+        "--build", "deps/raylib/build",
+        "--config", "Release",
+        "-j",
+    });
+
+    raylib_build.step.dependOn(&raylib_prebuild.step);
+
+    const exe = b.addExecutable(.{
+        .name = "doom-like",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.addIncludePath(.{ .path = "include" });
+    exe.addIncludePath(.{ .path = "deps/raylib/build/raylib/include" });
+    exe.addCSourceFiles(
+        &.{ "src/main.c" },
+        &.{
+            "-std=c99",
+            "-pedantic",
+            "-Wall",
+            "-Wextra",
+            "-Werror",
+        }
+    );
+
+    exe.linkLibC();
+    exe.addLibraryPath(.{ .path = "deps/raylib/build/raylib/Release" });
+    exe.linkSystemLibrary("raylib");
+
+    exe.linkSystemLibrary("opengl32");
+    exe.linkSystemLibrary("user32");
+    exe.linkSystemLibrary("gdi32");
+    exe.linkSystemLibrary("shell32");
+    exe.linkSystemLibrary("winmm");
+
+    exe.step.dependOn(&raylib_build.step);
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include "raylib.h"
+
+int main(void)
+{
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Hello, Sailor!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
